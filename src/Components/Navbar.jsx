aeb7909395f876548959b16d3e0aca44aac6ac83
PriyatoshKumarShahi <priytoshshahi90@gmail.com>
2024-07-25 19:29:32 +0530

    change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,7 +26,7 @@ export const Navbar = (props) => {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">
-                  Home
+                  About
                 </a>
               </li>
              
